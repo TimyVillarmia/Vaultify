795255c9e9001c6f502be788cd6f59eb604f2002
Timy Villarmia <timyvillarmia@gmail.com>
2024-04-13 13:48:44 +0800

    add signup MVP

--- Vaultify.Droid/Activities/ActivitySignUp.cs
+++ Vaultify.Droid/Activities/ActivitySignUp.cs
@@ -4,18 +4,21 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using AndroidX.AppCompat.App;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Vaultify.Droid.Common;
 using Vaultify.Droid.Common.IViews;
+using Vaultify.Droid.Presenters;
 
 namespace Vaultify.Droid.Activities
 {
     [Activity(Label = "ActivitySignUp")]
-    public class ActivitySignUp : Activity
+    public class ActivitySignUp : AppCompatActivity, ISignUpVew
     {
+        public event EventHandler SignUp;
 
         public void SignIn()
         {
@@ -31,10 +34,19 @@ namespace Vaultify.Droid.Activities
             // Create your application here
             SetContentView(Resource.Layout.signup);
 
+            // get the auth from the repository
+            FirebaseRepository firebaseRepository = new FirebaseRepository();
+            // start firebase
+
+
+            // pass the firebaseRepository object
+            // allows activities below get access all method of this
+            new SignUpPresenter(this, firebaseRepository);
+
         }
 
 
- 
+
 
 
     }
---
+++ Vaultify.Droid/Presenters/SignUpPresenter.cs
@@ -0,0 +1,41 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Vaultify.Droid.Common.IViews;
+using Vaultify.Droid.Common;
+
+namespace Vaultify.Droid.Presenters
+{
+    public class SignUpPresenter
+    {
+
+        private readonly IFirebase _firebaseRepository;
+        private readonly ISignUpVew _signUpActivity;
+        public SignUpPresenter()
+        {
+
+        }
+
+        public SignUpPresenter(ISignUpVew _view, IFirebase repository)
+        {
+            _firebaseRepository = repository;
+            _signUpActivity = _view;
+
+
+            _signUpActivity.SignUp += SignUpEvent;
+
+        }
+
+        private void SignUpEvent(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
