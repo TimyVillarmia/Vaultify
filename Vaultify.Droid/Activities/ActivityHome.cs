868017454ea7ebb5eca93304e3f61c73ae61b67a
Timy Villarmia <timyvillarmia@gmail.com>
2024-04-17 23:01:29 +0800

    test

--- Vaultify.Droid/Activities/ActivityHome.cs
+++ Vaultify.Droid/Activities/ActivityHome.cs
@@ -41,6 +41,7 @@ namespace Vaultify.Droid.Activities
         private void Toolbar_NavigationClick(object sender, Toolbar.NavigationClickEventArgs e)
         {
             drawerLayout.Open();
+            //test
         }
 
         public override bool OnCreateOptionsMenu(IMenu menu)
