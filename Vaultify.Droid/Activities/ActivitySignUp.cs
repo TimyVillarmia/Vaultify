e727ff170e32ecd6373f50a74b50fe4055def929
Timy Villarmia <timyvillarmia@gmail.com>
2024-04-27 17:50:44 +0800

    done sign up

--- Vaultify.Droid/Activities/ActivitySignUp.cs
+++ Vaultify.Droid/Activities/ActivitySignUp.cs
@@ -1,15 +1,23 @@
 ﻿using Android.App;
 using Android.Content;
+using Android.Gms.Common.Server.Response;
 using Android.OS;
 using Android.Runtime;
+using Android.Security;
+using Android.Text;
 using Android.Views;
 using Android.Widget;
 using AndroidX.AppCompat.App;
+using Firebase.Auth;
+using Google.Android.Material.Button;
+using Google.Android.Material.TextField;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using Vaultify.Droid.Common;
+using static Android.Provider.Telephony.Mms;
 
 namespace Vaultify.Droid.Activities
 {
@@ -17,6 +25,14 @@ namespace Vaultify.Droid.Activities
     public class ActivitySignUp : AppCompatActivity
     {
         TextView linkSignin;
+        TextInputLayout textFieldFullname;
+        TextInputLayout textFieldcreateEmail;
+        TextInputLayout textFieldcreatePass;
+        TextInputLayout textFieldconfirmPass;
+
+        MaterialButton btnSignup;
+
+        FirebaseAuth auth;
         public void SignIn()
         {
             throw new NotImplementedException();
@@ -34,8 +50,13 @@ namespace Vaultify.Droid.Activities
             // Create your application here
             SetContentView(Resource.Layout.signup);
             linkSignin = FindViewById<TextView>(Resource.Id.hyperlink_create);
+            textFieldFullname = FindViewById<TextInputLayout>(Resource.Id.textFieldFullname);
+            textFieldcreateEmail = FindViewById<TextInputLayout>(Resource.Id.textFieldcreateEmail);
+            textFieldcreatePass = FindViewById<TextInputLayout>(Resource.Id.textFieldcreatePass);
+            textFieldconfirmPass = FindViewById<TextInputLayout>(Resource.Id.textFieldconfirmPass);
+            btnSignup = FindViewById<MaterialButton>(Resource.Id.btnSignup);
 
-
+            auth = FirebaseRepository.getFirebaseAuth();
             // get the auth from the repository
             FirebaseRepository firebaseRepository = new FirebaseRepository();
             // start firebase
@@ -44,6 +65,104 @@ namespace Vaultify.Droid.Activities
 
 
             linkSignin.Click += LinkSignin_Click;
+            btnSignup.Click += BtnSignup_Click;
+
+       
+            textFieldFullname.EditText.TextChanged += delegate { ValidateField(textFieldFullname); };
+            textFieldcreateEmail.EditText.TextChanged += delegate { ValidateField(textFieldcreateEmail); };
+            textFieldcreatePass.EditText.TextChanged += delegate { ValidateField(textFieldcreatePass); };
+            textFieldconfirmPass.EditText.TextChanged += delegate { ValidateField(textFieldconfirmPass); };
+        }
+
+        private void ValidateField(TextInputLayout field)
+        {
+
+            
+            if (string.IsNullOrEmpty(field.EditText?.Text))
+            {
+                field.Error = "Must not be empty.";
+                btnSignup.Clickable = false;
+                return;
+
+            }
+            if (field.Id == textFieldFullname.Id)
+            {
+                if (field.EditText?.Text.Length < 4)
+                {
+                    textFieldFullname.Error = "Name too short. Please enter at least 4 characters.";
+                    btnSignup.Clickable = false;
+                    return;
+                }
+                else
+                {
+                    textFieldFullname.ErrorEnabled = false;
+                    btnSignup.Clickable = true;
+                    return;
+                }
+            }
+            if (field.Id == textFieldcreateEmail.Id)
+            {
+                bool isEmail = Regex.IsMatch(field.EditText?.Text, @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z", RegexOptions.IgnoreCase);
+
+                if (!isEmail)
+                {
+                    textFieldcreateEmail.Error = "Must be a valid email address";
+                    btnSignup.Clickable = false;
+                    return;
+                }
+                else
+                {
+                    textFieldcreateEmail.ErrorEnabled = false;
+                    btnSignup.Clickable = true;
+                    return;
+                }
+            }
+            if (field.Id == textFieldcreatePass.Id)
+            {
+
+                if (field.EditText?.Text.Length < 8)
+                {
+                    textFieldcreatePass.Error = "Password must be 8-10 characters.";
+                    btnSignup.Clickable = false;
+                    return;
+                }
+            }
+            if (field.Id == textFieldconfirmPass.Id)
+            {
+
+                if (field.EditText?.Text != textFieldcreatePass.EditText?.Text)
+                {
+                    textFieldconfirmPass.Error = "Passwords don't match.";
+                    btnSignup.Clickable = false;
+                    return;
+                }
+            }
+
+
+
+        }
+
+
+
+
+        private void BtnSignup_Click(object sender, EventArgs e)
+        {
+
+
+            string fullname = textFieldFullname.EditText?.Text;
+             string email = textFieldcreateEmail.EditText?.Text;
+             string pass = textFieldcreatePass.EditText?.Text;
+             string confirmpass = textFieldconfirmPass.EditText?.Text;
+
+            if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(pass))
+            {
+      
+                return;
+            }
+
+            auth.CreateUserWithEmailAndPassword(email, pass);
+
+
 
         }
 
